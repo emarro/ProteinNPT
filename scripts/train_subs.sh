81c3bb0fd460762a0a6e43ba76576b7acbb8aca7
Edgar Marroquin <emarro@cs.cornell.edu>
2024-06-20 19:06:57 -0400

    Update default training launcher for slurm

--- scripts/train_subs.sh
+++ scripts/train_subs.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=2
+#SBATCH -J 3M_625_1500_seqs                 # Job name
+#SBATCH -o /share/kuleshov/emm392/ProteinNPT/watch_logs/3M_subs_%j.out                  # Name of stdout output log file
+#SBATCH -e /share/kuleshov/emm392/ProteinNPT/watch_logs/3M_subs_%j.err                  # Name of stderr output log file
+#SBATCH -t 72:00:00                          # Time limit (hh:mm:ss)
+#SBATCH --partition=gpu             # Request partition for resource allocation
+#SBATCH --constraint="gpu-mid|gpu-high"
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --open-mode=append
+#SBATCH --requeue
+#SBATCH --array=22,48
+# SBATCH --array=22,27,48,53,116,117,118,158,161,196
+
 source ./config.sh
 conda activate proteinnpt_env
 
@@ -5,8 +20,12 @@ export model_config_location=$MambaNPT_config_location # [ProteinNPT_config_loca
 export sequence_embeddings_folder=$MSAT_embeddings_folder # [MSAT_embeddings_folder|Tranception_embeddings_folder|ESM1v_embeddings_folder]
 
 export fold_variable_name='fold_random_5' #[fold_random_5 | fold_contiguous_5 | fold_modulo_5]
-export assay_index=1 #Replace with index of desired DMS assay in the ProteinGym reference file (`utils/proteingym`)
-export model_name_suffix='All_singles_final_test' #Give a name to the model
+export assay_index=$SLURM_ARRAY_TASK_ID #Replace with index of desired DMS assay in the ProteinGym reference file (`utils/proteingym`)
+export train_num_assay=425  #(labeld) assay sequeces  during training
+export eval_num_training=1000 #(labled) assay sequences during inference
+export num_msa_training=384 #384 #$msa sequences during training
+export model_name_suffix='singles_3M_mnpt_'${train_num_assay}'_train_'${eval_num_training}'_eval_seqs_assay_'$assay_index #Give a name to the model
+
 
 python train.py \
     --data_location ${proteinnpt_data_path} \
@@ -18,4 +37,11 @@ python train.py \
     --zero_shot_fitness_predictions_location ${zero_shot_fitness_predictions_substitutions} \
     --training_fp16 \
     --sequence_embeddings_folder ${sequence_embeddings_folder} \
-    --model_name_suffix ${model_name_suffix}
+    --model_name_suffix ${model_name_suffix} \
+    --use_wandb \
+    --embed_dim 200 \
+    --num_protein_npt_layer 3 \
+    --training_num_assay_sequences_per_batch_per_gpu ${train_num_assay} \
+    --eval_num_training_sequences_per_batch_per_gpu ${eval_num_training} \
+    --num_MSA_sequences_per_training_instance ${num_msa_training} \
+    --max_learning_rate 0.0001
